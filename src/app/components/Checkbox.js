302d5599a1a777e2d5fb723aa95582b35c88aa1b
u7384629 <u7384629@anu.edu.au>
2023-08-24 14:38:33 +1000

    initial commit

---
+++ src/app/components/Checkbox.js
@@ -0,0 +1,32 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Checkbox = ({ label, checked = false, onChange, color, variables }) => (
+    <div style={{margin: 10}}>
+        <button style={{
+            width: 24, 
+            height: 24, 
+            borderWidth: 1, 
+            bodorColor: "blue", 
+            backgroundColor: checked ? color : "white",
+            marginRight: 10,
+            boxShadow: "4px 4px #b5b5b5",
+            borderRadius: 4
+        }} 
+            onClick={() => onChange(label, checked)}/>
+        <span>{label}</span>
+        {checked && variables.map(variable => <p key={variable} style={{fontSize:11, marginLeft:34, marginTop:3, marginBottom:0}}>{variable}</p>)}
+    </div>
+  
+);
+
+Checkbox.propTypes = {
+  label: PropTypes.string.isRequired,
+  checked: PropTypes.bool,
+  color: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  variables: PropTypes.array
+}
+
+export default Checkbox;
