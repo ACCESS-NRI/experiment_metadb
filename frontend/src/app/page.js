5ee7957bb12b5b3949fc956680146fe30ce15b2f
Davide Marchegiani <davide.marchegiani@gmail.com>
2024-09-11 17:18:57 +1000

    Add landing page (#20)
    Added landing page
    
    Co-authored-by: Aidan Heerdegen <aidan.heerdegen@gmail.com>

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,22 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import classes from './page.module.css';
+
 export default function Home() {
   return (
-    
-   <div>Access Nri data explorer</div>
+  <>
+    <div className={classes.topBanner}>
+      <div className={classes.logo}>
+        <Image src="/ACCESS-NRI_logo.png" alt="ACCESS-NRI_logo" fill />
+      </div>
+      <div className={`${classes.title} ${classes.dongle}`}>
+        ACCESS-NRI Data Explorer (experimental prototype)
+      </div>
+    </div>
+    <div className={`${classes.cardContainer} ${classes.dongle}`}>
+      <Link href="/release-provenance" className={classes.card}>Model Build Database</Link>
+      <Link href="/catelog" className={classes.card}>Experiments Catalog</Link>
+    </div>
+  </>
   )
 }
